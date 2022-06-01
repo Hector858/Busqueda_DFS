fcb3b1b67088ce23254857e9e828da9a55724c21
Hector858 <hicedeno@espe.edu.ec>
2022-05-31 21:43:51 -0500

    Creación del método para imprimir lista de adyacencia

--- DFS.py
+++ DFS.py
@@ -13,6 +13,16 @@ class Grafo:
             indica el número de nodos que va a llevar el grafo
     dirigido : boolean
             Si el grafo se encuentra en dirigido (True) o no dirigido (False)
+    nodo1 : int 
+            Nodo 1 o de inicio
+    nodo2 : int
+            Nodo 2 o de fin
+    peso : int 
+            Peso de la arista
+            
+            Este atributo puede tener un valor opcional (proporcionado por uno mismo)
+    nodo_inicio : int  
+            Nodo inicial del recorrido
             
     Métodos
     -------
@@ -24,6 +34,10 @@ class Grafo:
         Método que agrega una arista al grafo.
         
         Si el primer grafo no se encuentra dirigido se pasa al siguiente
+    imprimir_lista_adyacencia(self):
+        Método que imprime la lista de adyacencia.
+        
+        Imprime la lista de adyacencia de los nodos por cada una de sus llaves.
     """
     # Constructor
     def __init__(self, numero_de_nodos, dirigido=True):#self es uno mismo
@@ -79,3 +93,16 @@ class Grafo:
         if not self.m_dirigido:
             #Agregar una arista del nodo 2 de la lista de adyacencia
             self.m_lista_adyacencia[nodo2].add((nodo1, peso))  
+           
+    # Función que imprime la representación del grafo
+    def imprimir_lista_adyacencia(self):
+        ''' 
+        Método que imprime la lista de adyacencia por medio de una matriz.
+        
+        Imprime la lista de adyacencia de los nodos por cada una de sus llaves.
+        '''
+        #Realiza un recorrido en la lista de adyacencia a través de sus llaves
+        for llave in self.m_lista_adyacencia.keys():
+            #Imprime cada uno de los nodos que se encuentren en la lista de adyacencia
+            #colocan donde se dirigen los nodos y su peso
+            print("nodo", llave, ": ", self.m_lista_adyacencia[llave])
